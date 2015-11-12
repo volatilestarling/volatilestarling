efc427cc51bc4e9456cb4458f4eb63e78d27674e
Jira Vinyoopongphan <jira.vinyoo@gmail.com>
2015-11-11 20:32:23 -0800

    (refactor) adding token authentication

--- client/app/app.js
+++ client/app/app.js
@@ -22,3 +22,23 @@ angular.module('whereto', ['whereTo.map', 'where-to.services', 'whereTo.auth', '
     $urlRouterProvider.otherwise('/map');
 
 }])
+.factory('AttachTokens', function ($window) {
+  var attach = {
+    request: function (object) {
+      var jwt = $window.localStorage.getItem('com.shortly');
+      if (jwt) {
+        object.headers['x-access-token'] = jwt;
+      }
+      object.headers['Allow-Control-Allow-Origin'] = '*';
+      return object;
+    }
+  };
+  return attach;
+})
+.run(function ($rootScope, $location, Auth) {
+  $rootScope.$on('$routeChangeStart', function (evt, next, current) {
+    if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
+      $location.path('/signin');
+    }
+  });
+});
