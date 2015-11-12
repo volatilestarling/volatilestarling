896889fc9dbb50c0d1948422bd6770f178699239
Jira Vinyoopongphan <jira.vinyoo@gmail.com>
2015-11-11 20:57:54 -0800

    (refactor) added auth factory and refactored auth controller for token auth

--- client/app/controllers/authController.js
+++ client/app/controllers/authController.js
@@ -1,62 +1,31 @@
 angular.module('whereTo.auth', ['firebase'])
-  .controller('AuthController', ['$scope', '$state', '$stateParams', function($scope, $state, $stateParams) {
-      var fbRef = new Firebase("URL HERE");
+.controller('AuthController', ['$scope', '$state', 'Auth', function($scope, $state, Auth) {
 
-      var checkAuth = new FirebaseSimpleLogin(fbRef, function(err, user) {
-          if (err !== null) {
-              console.log("Error authenticating:", err);
-          } else if (user !== null) {
-              console.log("User is logged in:", user);
-          } else {
-              console.log("User is logged out");
-          }
-      });
+  //on authentication, redirect to map
+  $scope.user = {};
 
-      fbRef.onAuth(function(authData) {
-          if (authData) {
-              $state.go('map')
-          } else {
-              console.log('User unauthenticated')
-          }
+  $scope.signin = function () {
+    Auth.signin($scope.user)
+      .then(function (token) {
+        $window.localStorage.setItem('com.whereto', token);
+        $state.go('map');
       })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
 
-      $scope.login = function() {
-          var email = $scope.user.email;
-          var password = $scope.user.password;
+  $scope.signup = function () {
+    Auth.signup($scope.user)
+      .then(function (token) {
+        $window.localStorage.setItem('com.whereto', token);
+        $location.path('/links');
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };  
 
-          checkAuth.login('password', {
-              email: email,
-              password: password
-          })
-      }
+  //signout?
 
-      $scope.signup = function() {
-          var email = $scope.user.email;
-          var password = $scope.user.password;
-
-          var userRef = new Firebase("URL HERE")
-
-          checkAuth.createUser(email, password, function(error, user) {
-              if (error === null) {
-                  console.log("User created successfully:", user);
-                  $scope.login();
-                  //saving user info
-                  userRef.child(user.uid).set({
-                      provider: user.provider,
-                      name: user.email,
-                      whereToList: []
-                  });
-
-              } else {
-                  console.log("Error creating user:", error);
-              }
-          });
-
-      }
-
-      $scope.logout = function() {
-          checkAuth.logout();
-          $state.go('login')
-      }
-
-  }])
+}])
--- client/app/services/services.js
+++ client/app/services/services.js
@@ -11,6 +11,46 @@ angular.module('where-to.services', [])
 
   return googleMapService;
 })
+.factory('Auth', function ($http, $location, $window) {
+  var signin = function (user) {
+    return $http({
+      method: 'POST',
+      url: '/api/users/signin',
+      data: user
+    })
+    .then(function (resp) {
+      return resp.data.token;
+    });
+  };
+
+  var signup = function (user) {
+    return $http({
+      method: 'POST',
+      url: '/api/users/signup',
+      data: user
+    })
+    .then(function (resp) {
+      return resp.data.token;
+    });
+  };
+
+  var isAuth = function () {
+    return !!$window.localStorage.getItem('com.whereto');
+  };
+
+  var signout = function () {
+    $window.localStorage.removeItem('com.whereto');
+    $state.go('login');
+  };
+
+
+  return {
+    signin: signin,
+    signup: signup,
+    isAuth: isAuth,
+    signout: signout
+  };
+});
 // .factory('DataService', function($http) {
 //   var getLocations = function () {
 //     return $http({
