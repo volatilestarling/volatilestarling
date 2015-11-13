ab5cd7486408f6565c13271aedfaf764bcdf6055
Kaijie Zheng <kaijiejz50@gmail.com>
2015-11-12 18:15:33 -0800

    (Feat) Change server variables for deployment

--- server/server.js
+++ server/server.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var uriUtil = require('mongodb-uri');
 
 var port  = process.env.PORT || 3000;
 var app = express();
 
-mongoose.connect('mongodb://localhost/yugen');
+var options = {
+  server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
+  replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } }
+};
+
+var mongodbUri = 'mongodb://heroku_q2tbs7nz:vip6tj89t803bf8vnala6oa9g8@ds053964.mongolab.com:53964/heroku_q2tbs7nz';
+var mongooseUri = uriUtil.formatMongoose(mongodbUri);
+
+mongoose.connect(mongooseUri, options);
 
 // require('server/routes/routes.js')(app);
 require('./config/middleware.js')(app, express);
