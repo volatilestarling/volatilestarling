47e8643211b18c7c4d4eb8cb26f950f43cef41f1
Jira Vinyoopongphan <jira.vinyoo@gmail.com>
2015-11-13 15:14:24 -0800

    (fix) added missing methods to userSchema

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,5 +1,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');
+var Q = require('q');
 
 var userSchema = new Schema({
   username: {
@@ -22,6 +24,43 @@ var userSchema = new Schema({
     */
     type: Schema.Types.Mixed
   }
+});
+
+userSchema.methods.comparePasswords = function (candidatePassword) {
+  var defer = Q.defer();
+  var savedPassword = this.password;
+  bcrypt.compare(candidatePassword, savedPassword, function (err, isMatch) {
+    if (err) {
+      defer.reject(err);
+    } else {
+      defer.resolve(isMatch);
+    }
+  });
+  return defer.promise;
+};
+
+userSchema.pre('save', function (next) {
+  var user = this;
+
+  if (!user.isModified('password')) {
+    return next();
+  }
+
+  bcrypt.genSalt(10, function (err, salt) {
+    if (err) {
+      return next(err);
+    }
+
+    bcrypt.hash(user.password, salt, null, function (err, hash) {
+      if (err) {
+        return next(err);
+      }
+
+      user.password = hash;
+      user.salt = salt;
+      next();
+    });
+  });
 });
 
 var User = mongoose.model('User', userSchema);
