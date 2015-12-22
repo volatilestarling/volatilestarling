9a2d02f4a1a83aee24815da14b6cfb6de2f2351c
Jira Vinyoopongphan <jira.vinyoo@gmail.com>
2015-12-22 11:24:27 -0800

    (fix) Included an additional route to location post request to handle state info

--- server/routes/locationRoutes.js
+++ server/routes/locationRoutes.js
@@ -2,5 +2,5 @@ var locationController = require('./locationController.js');
 
 module.exports = function (app) {
   app.get('/', locationController.retrieveData);
-  app.post('/', locationController.addCity);
+  app.post('/', locationController.addCity, locationController.addStateInfo);
 };
