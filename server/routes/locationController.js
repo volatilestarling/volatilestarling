955c3bca74302546c5362fa7fb314e8f307854f9
Jira Vinyoopongphan <jira.vinyoo@gmail.com>
2015-12-22 11:26:39 -0800

    (fix) Added function to retrieve state travel info

--- server/routes/locationController.js
+++ server/routes/locationController.js
@@ -5,143 +5,204 @@ var cheerio = require('cheerio');
 var request = require('request');
 
 module.exports = {
-  retrieveData: function (req, res, next) {
-    // Sample GET request from front end: /api/location?city=shanghai&country=china&user=kaijie@gmail.com
-    console.log('params', req.query)
-    var username = req.query.user;
-    var city = req.query.city;
-    var country = req.query.country;
+	retrieveData: function (req, res, next) {
+		// Sample GET request from front end: /api/location?city=shanghai&country=china&user=kaijie@gmail.com
+		console.log('params', req.query)
+		var username = req.query.user;
+		var city = req.query.city;
+		var country = req.query.country;
 
-    var url = 'http://travel.state.gov/content/passports/en/country/' + country.toLowerCase() + '.html';
+		var url = 'http://travel.state.gov/content/passports/en/country/' + country.toLowerCase() + '.html';
 
-    var findCity = Q.nbind(Location.findOne, Location);
-    findCity({ city: city })
-      .then(function (location) {
-        if (!location) {
-          next(new Error('Location does not exist'));
-        } else {
-          request(url, function(error, response, html){
-            if(!error){
-              var $ = cheerio.load(html);
-              var data = {
-                location: location,
-                info: {}
-              };
+		var findCity = Q.nbind(Location.findOne, Location);
+		findCity({city: city})
+			.then(function (location) {
+				if (!location) {
+					next(new Error('Location does not exist'));
+				} else {
+					//request(url, function (error, response, html) {
+					//	if (!error) {
+					//		var $ = cheerio.load(html);
+					//		var data = {
+					//			location: location
+					//		};
+					//
+					//	var getInfo = Q.nfcall(module.exports.fetchStateInfo, country);
+					//
+					//	getInfo
+					//		.then(function(info) {
+					//			data.info = JSON.parse(info.results);
+					//			res.status(200).send(data);
+					//		})
 
-              for (var i = 1; i <= 6; i++) {
-                var selector = '.quick_fact' + i;
+							//for (var i = 1; i <= 6; i++) {
+							//	var selector = '.quick_fact' + i;
+							//
+							//	$(selector).filter(function () {
+							//		var content = $(this);
+							//		data[content.children().first().text()] = content.children().last().text();
+							//	});
+							//}
+							//console.log('deets', data)
+							res.status(200).send(location);
+						//}
+					//});
+				}
+			})
+			.fail(function (error) {
+				next(error);
+			});
+	},
+	addCity: function (req, res, next) {
+		console.log('addCity', req.body)
+		var place = req.body.place;
+		var city = req.body.city;
+		var country = req.body.country;
+		var username = req.body.user;
 
-                $(selector).filter(function () {
-                  var content = $(this);
-                  data[content.children().first().text()] = content.children().last().text();
-                });
-              }
-              console.log('deets', data)
-              res.status(200).send(location);
-            }
-          });
-        }
-      })
-      .fail(function (error) {
-        next(error);
-      });
-  },
-  addCity: function (req, res, next) {
-    console.log('addCity', req.body)
-    var place = req.body.place;
-    var city = req.body.city;
-    var country = req.body.country;
-    var username = req.body.user;
-    // need to fetch attractions from trip advisor api
+		// need to add location to userSchema too
+		var findUser = Q.nbind(User.findOne, User);
+		var findCity = Q.nbind(Location.findOne, Location);
 
-    // need to add location to userSchema too
-    var findUser = Q.nbind(User.findOne, User);
-    var findCity = Q.nbind(Location.findOne, Location);
+		console.log('adding city now');
 
-    console.log('adding city now');
-    console.log(username, place);
+		findUser({username: username})
+			.then(function (user) {
+				if (!user) {
+					next(new Error('User does not exist'));
+				} else {
+					findCity({city: city, country: country})
+						.then(function (location) {
 
-    findUser({ username: username })
-      .then(function (user) {
-        if (!user) {
-          next(new Error('User does not exist'));
-        } else {
-          findCity({ city: city, country: country })
-            .then(function (location) {
-console.log('finding city')
-              var newLocation;
-              if (!location) {
-                var create = Q.nbind(Location.create, Location);
-
-                newLocation = {
-                  city: city,
-                  country: country,
-                  attractions: []
-                };
-
-                var fetchCB = function(err, response, body) {
-                  //array of attractions (objects)
-                  var toDoList = JSON.parse(body);
-    
-                  for(var place in toDoList) { 
-                    newLocation.attractions.push(toDoList[place])
-                  }
-                  create(newLocation);
-                };
+							var newLocation;
+							if (!location) {
+								var create = Q.nbind(Location.create, Location);
+								console.log('creating city')
+								newLocation = {
+									city: city,
+									country: country,
+									attractions: [],
+									info: null
+								};
 
 
-                if(newLocation.city === "") {
-                  module.exports.fetchAttractions(newLocation.country, fetchCB);
-                } else {
-                  module.exports.fetchAttractions(newLocation.country, fetchCB, newLocation.city);
-                }
-        
-              // double check model to see if we should query city and/or country
-              if (!user.locations[place]) {
-                // initialize attractions array only if location not already added
-                user.locations[place] = [];
-              }
-              location = location ? newLocation : location;
-              res.status(200).send(location);
-            } else {
-              console.log('location already exists');
-            }
-          });
-        }
-      })
-      .fail(function (error) {
-        next(error);
-      });
-  },
+								var fetchCB = function (err, response, body) {
+									//array of attractions (objects)
+									var toDoList = JSON.parse(body);
 
-  fetchAttractions: function (country, callback, city) {
-    //consider white space cases
-    country = country.replace(/\s/g, '-');
-    var url;
+									for (var place in toDoList) {
+										newLocation.attractions.push(toDoList[place])
+									}
 
-    if(city === undefined) {
-      url = "http://www.kimonolabs.com/api/ondemand/6fndw1y6?apikey=ZFsa5estp95igf9lGTKDK8u2iuAfzbOW&kimpath1=" + country + "&kimmodify=1";
-    } else {
-      city = city.replace(/\s/g, '-');
-      url = "https://www.kimonolabs.com/api/ondemand/eg6pzbee?apikey=ZFsa5estp95igf9lGTKDK8u2iuAfzbOW&kimpath1=" + country + "&kimpath2=" + city + "&kimmodify=1";
-    }
+									create(newLocation)
+								};
 
-    request(url, function(err, response, body) {
-      if(err) {
-        console.error(err);
-      } else {
-        /*
-        body is obj with following structure:
-        "0": {
-            "Attraction": "National Palace Museum",
-            "Description": "Home to the world's largest and arguably finest collection of Chinese art, this vast collection covers treasures in painting, calligraphy, statuary, bronzes, laquerware, ceramics, jade and religious objects...",
-            "City": "Taipei",
-            "Type": "Museums & Galleries",
-            "Link": "http://www.lonelyplanet.com/australia/sights/historic/historic-port-echuca/item-a-1164731-id"
-          }
-        */
-        callback(err, response, body);
-      }
-    })
-  }
-};
+								if (newLocation.city === "") {
+									module.exports.fetchAttractions(newLocation.country, fetchCB);
+								} else {
+									module.exports.fetchAttractions(newLocation.country, fetchCB, newLocation.city);
+								}
+
+
+								// double check model to see if we should query city and/or country
+								if (!user.locations[place]) {
+									// initialize attractions array only if location not already added
+									user.locations[place] = [];
+								}
+								location = location ? newLocation : location;
+								res.status(200).send(location);
+							} else {
+								console.log('location already exists');
+							}
+						});
+				}
+				setTimeout(function() {
+					next();
+				}, 10000)
+			})
+			.fail(function (error) {
+				next(error);
+			});
+	},
+
+	addStateInfo: function(req, res) {
+		var country = req.body.country;
+
+		var getInfo = Q.nfcall(module.exports.fetchStateInfo, country);
+		var findCity = Q.nbind(Location.findOne, Location);
+
+		findCity({country: country})
+			.then(function(location) {
+				getInfo
+					.then(function(info) {
+						location.info = JSON.parse(info.body).results['QUICK FACTS'][0];
+						location.save();
+					})
+			})
+			.then(function() {
+				res.status(201)
+			})
+			.catch(function(err) {
+				console.error('Error in fetching state info', err)
+			})
+
+	},
+	/**
+	 * Makes call to Kimono Scraping API for Lonely Planet information based on city and country or country.
+	 * @param country
+	 * @param callback
+	 * @param city
+	 * @returns example body of response:
+	 * "0": {
+				 "Attraction": "National Palace Museum",
+				 "Description": "Home to the world's largest and arguably finest collection of Chinese art, this vast collection covers treasures in painting, calligraphy, statuary, bronzes, laquerware, ceramics, jade and religious objects...",
+				 "City": "Taipei",
+				 "Type": "Museums & Galleries",
+				 "Link": "http://www.lonelyplanet.com/australia/sights/historic/historic-port-echuca/item-a-1164731-id"
+				 }
+	 */
+
+	fetchAttractions: function (country, callback, city) {
+		//consider white space cases
+		country = country.replace(/\s/g, '-');
+		var url;
+
+		if (city === undefined) {
+			url = "http://www.kimonolabs.com/api/ondemand/6fndw1y6?apikey=ZFsa5estp95igf9lGTKDK8u2iuAfzbOW&kimpath1=" + country + "&kimmodify=1";
+		} else {
+			city = city.replace(/\s/g, '-');
+			url = "https://www.kimonolabs.com/api/ondemand/eg6pzbee?apikey=ZFsa5estp95igf9lGTKDK8u2iuAfzbOW&kimpath1=" + country + "&kimpath2=" + city + "&kimmodify=1";
+		}
+
+		request(url, function (err, response, body) {
+			if (err) {
+				console.error(err);
+			} else {
+				callback(err, response, body);
+			}
+		})
+	},
+	/**
+	 * Makes call to Kimono Scraping API for information from travel.state.gov
+	 * @param country
+	 * @param callback
+	 * @returns Response from call will be an object that contains a results property holding necessary information listed under 'QUICK FACTS'
+	 *  { 'PASSPORT VALIDITY': 'Must be valid for at least three months beyond your planned date of departure from the Schengen area.',
+      VACCINATIONS: 'None',
+     'BLANK PASSPORT PAGES': 'One page required for entry stamp',
+     'TOURIST VISA REQUIRED': 'Not required for stays less than 90 days in Spain.  Not required for stays less than three months in Andorra.',
+     'CURRENCY RESTRICTIONS FOR ENTRY': 'None',
+     'CURRENCY RESTRICTIONS FOR EXIT': 'None' },
+	 */
+	fetchStateInfo: function (country, callback) {
+		var url = "http://www.kimonolabs.com/api/ondemand/ebbvwtwk?apikey=ZFsa5estp95igf9lGTKDK8u2iuAfzbOW&kimpath5=" + country + ".html";
+
+		request(url, function (err, response) {
+			if(err) {
+				console.error(err);
+			} else {
+				callback(err, response)
+			}
+		})
+	}
+}
